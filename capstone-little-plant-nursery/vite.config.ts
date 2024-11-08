1a8efeff211911140b100f9b67c0f6e1fc855cf9
Kojirou <54878605+KameiKojirou@users.noreply.github.com>
2024-11-07 23:36:27 -0700

    move up a directory

--- capstone-little-plant-nursery/vite.config.ts
+++ capstone-little-plant-nursery/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/KameiKojirou/capstone-little-nursery/",
+  base: "/KameiKojirou/capstone-little-nursery/capstone-little-nursery/",
   plugins: [react()],
 })
